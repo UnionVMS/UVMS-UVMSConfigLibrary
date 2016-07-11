e99d92abb57519f338521c77cf70c28298cdb1c1
Joakim Johansson <joakimjohansson@outlook.com>
2016-07-11 13:33:17 +0200

    Added Weblogic support
    Removed ManagedScheduledExecutorService and replaced with @Schedule.
    This is becase WEblogic 12.1.3 does not support ManagedExecutorService.
    https://community.oracle.com/thread/3602407?start=0&tstart=0

--- src/main/java/eu/europa/ec/fisheries/uvms/config/service/PingTimer.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/config/service/PingTimer.java
@@ -1,22 +1,8 @@
-/*
-﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
-© European Union, 2015-2016.
-
-This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
-the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
-copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
- */
 package eu.europa.ec.fisheries.uvms.config.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -24,25 +10,36 @@ import org.slf4j.LoggerFactory;
 
 import eu.europa.ec.fisheries.uvms.config.exception.ConfigServiceException;
 
-import java.util.concurrent.TimeUnit;
-
 @Singleton
 @Startup
 public class PingTimer {
 
+    /**
+     * https://blogs.oracle.com/WebLogicServer/entry/concurrency_utilities_support_in_weblogic2
+     * https://community.oracle.com/thread/3602407?start=0&tstart=0 => NO
+     * support for Managed ExecutorService in WebLogic 12.1.3
+     */
     final static Logger LOG = LoggerFactory.getLogger(PingTimer.class);
 
     @Inject
     UVMSConfigService configService;
 
-    @Resource(lookup="java:/uvmsConfigPingExecutorService")
-    private ManagedScheduledExecutorService executorService;
-
-    @PostConstruct
+    @Schedule(second = "0", minute = "*/5", hour = "*", persistent = false)
     public void sendPing() {
-        LOG.info("PingTimer init");
-        PingTask checkCommunicationTask = new PingTask(configService);
-        executorService.scheduleWithFixedDelay(checkCommunicationTask, 0, 5, TimeUnit.MINUTES);
+        try {
+            LOG.info("Ping time arrived!");
+            configService.sendPing();
+        } catch (ConfigServiceException e) {
+            LOG.error("[ Error when sending ping to Config. ] {}", e.getMessage());
+        }
     }
 
+    //@Resource(mappedName = "uvmsConfigPingExecutorService")
+    //private ManagedScheduledExecutorService executorService;
+    //@PostConstruct
+    //public void sendPing() {
+    //    LOG.info("PingTimer init");
+    //PingTask checkCommunicationTask = new PingTask(configService);
+    //executorService.scheduleWithFixedDelay(checkCommunicationTask, 0, 5, TimeUnit.MINUTES);
+    //}
 }
