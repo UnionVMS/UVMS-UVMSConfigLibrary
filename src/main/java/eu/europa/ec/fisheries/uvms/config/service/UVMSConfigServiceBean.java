a126a77f0569ea8458007629f50c3bc75b41145d
ANKOVI <fevamko@yahoo.com>
2019-02-12 11:22:08 +0100

    Update

--- src/main/java/eu/europa/ec/fisheries/uvms/config/service/UVMSConfigServiceBean.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/config/service/UVMSConfigServiceBean.java
@@ -32,18 +32,17 @@ import eu.europa.ec.fisheries.uvms.config.model.mapper.ModuleResponseMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
+import javax.ejb.*;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-@Stateless
+@Singleton
+@Transactional
 public class UVMSConfigServiceBean implements UVMSConfigService {
 
     private final static Logger LOG = LoggerFactory.getLogger(UVMSConfigServiceBean.class);
@@ -173,6 +172,7 @@ public class UVMSConfigServiceBean implements UVMSConfigService {
     }
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    @Lock(LockType.WRITE)
     private void storeSettings(List<SettingType> settings) throws ConfigServiceException {
         parameterService.clearAll();
         for (SettingType setting: settings) {
